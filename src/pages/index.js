acae64d55b535af96c3365c5681b7185c023196d
auroraptor <interestingbutno@yandex.ru>
2022-11-09 21:26:39 +0300

    feat: добавить обработчик Кнопке 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,3 +5,24 @@ const buttonWarning = page.querySelector('#button_type_warning');
 buttonWarning.addEventListener('click', () => {
   header.classList.toggle('header_hide');
 });
+
+$(function(){
+  jQuery.fn.swap = function(b) {
+    b = jQuery(b)[0];
+    var a = this[0],
+        a2 = a.cloneNode(true),
+        b2 = b.cloneNode(true),
+        stack = this;
+
+    a.parentNode.replaceChild(b2, a);
+    b.parentNode.replaceChild(a2, b);
+
+    stack[0] = a2;
+    return this.pushStack( stack );
+};
+
+$('button').on('click', function(){
+  $('.left').swap('.right');
+});
+  });
+
