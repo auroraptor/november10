9f5eff3ebf594122dfe6b128d4f0fc61de802a08
auroraptor <interestingbutno@yandex.ru>
2022-11-10 15:17:31 +0300

    fix: remove utils

--- utils/constants.js
+++
@@ -1,8 +0,0 @@
-const page = document.querySelector('.page');
-const header = page.querySelector('.header');
-const buttonWarning = page.getElementById('button_type_warning');
-
-console.log(page);
-
-
-export { page, header, buttonWarning }
