1bda7474e4e7066233f17ee104d6d362bad5ec9a
auroraptor <interestingbutno@yandex.ru>
2022-11-10 14:33:03 +0300

    feat: добавить асинхронный get-запрос и стили формы

--- pages/form.js
+++ pages/form.js
@@ -1,14 +1,15 @@
-console.log('hello');
 const page = document.querySelector('.page');
 const form = page.querySelector('.form');
+const container = page.querySelector('.json__container');
 const select = document.querySelector('#select-template').content;
-
 const OPTION = 5;
 const SELECT = 5;
 
-const createSelect = (options, id) => {
+const createSelect = (options, name, id) => {
   const selectElement = select.querySelector('.select').cloneNode(true);
+  selectElement.setAttribute('name', name);
   selectElement.setAttribute('id', id);
+
   for (i = 1; i <= options; i++) {
     const optionTag = document.createElement('option');
     optionTag.textContent = i;
@@ -23,10 +24,30 @@ const render = (element, container) => {
 
 const repeat = (n, render) => {
   for (let i = 0; i < n; i++) {
-    render(createSelect(OPTION, `select_${n-i}`), form);
+    render(createSelect(OPTION, `name_${n-i}`, `#${n-i}`), form);
   }
+}
+
+const serializeForm = (formNode) => {
+  const { elements } = formNode;
+  const data = Array.from(elements)
+    .filter((item) => !!item.name)
+    .map((element) => {
+      const { name, value } = element;
+      return { name, value };
+  });
+  return data;
 }
 
 repeat(SELECT, render);
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  container.textContent = JSON.stringify({ ...serializeForm(e.target) });
+  fetch('https://api.kanye.rest')
+  .then(res => res.json())
+  .then((data) => {
+    alert(data.quote);
+  });
+});
 
