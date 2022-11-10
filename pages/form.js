5609b48cdfe2d7eed2742adfb8598572b81a9a39
auroraptor <interestingbutno@yandex.ru>
2022-11-10 15:28:34 +0300

    prettier

--- pages/form.js
+++ pages/form.js
@@ -1,32 +1,32 @@
-const page = document.querySelector('.page');
-const form = page.querySelector('.form');
-const container = page.querySelector('.json__container');
-const select = document.querySelector('#select-template').content;
+const page = document.querySelector(".page");
+const form = page.querySelector(".form");
+const container = page.querySelector(".json__container");
+const select = document.querySelector("#select-template").content;
 const OPTION = 5;
 const SELECT = 5;
 
 const createSelect = (options, name, id) => {
-  const selectElement = select.querySelector('.select').cloneNode(true);
-  selectElement.setAttribute('name', name);
-  selectElement.setAttribute('id', id);
+  const selectElement = select.querySelector(".select").cloneNode(true);
+  selectElement.setAttribute("name", name);
+  selectElement.setAttribute("id", id);
 
   for (i = 1; i <= options; i++) {
-    const optionTag = document.createElement('option');
+    const optionTag = document.createElement("option");
     optionTag.textContent = i;
     selectElement.append(optionTag);
   }
   return selectElement;
-}
+};
 
 const render = (element, container) => {
   container.prepend(element);
-}
+};
 
 const repeat = (n, render) => {
   for (let i = 0; i < n; i++) {
-    render(createSelect(OPTION, `name_${n-i}`, `#${n-i}`), form);
+    render(createSelect(OPTION, `name_${n - i}`, `#${n - i}`), form);
   }
-}
+};
 
 const serializeForm = (formNode) => {
   const { elements } = formNode;
@@ -35,19 +35,18 @@ const serializeForm = (formNode) => {
     .map((element) => {
       const { name, value } = element;
       return { name, value };
-  });
+    });
   return data;
-}
+};
 
 repeat(SELECT, render);
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   container.textContent = JSON.stringify({ ...serializeForm(e.target) });
-  fetch('https://api.kanye.rest')
-  .then(res => res.json())
-  .then((data) => {
-    alert(data.quote);
-  });
+  fetch("https://api.kanye.rest")
+    .then((res) => res.json())
+    .then((data) => {
+      alert(data.quote);
+    });
 });
-
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,51 @@
-const page = document.querySelector('.page');
-const header = page.querySelector('.header');
-const buttonWarning = page.querySelector('#button_type_warning');
-const popup = page.querySelector('.popup');
-const closeIcon = popup.querySelector('.popup__close-icon');
+const page = document.querySelector(".page");
+const header = page.querySelector(".header");
+const buttonWarning = page.querySelector("#button_type_warning");
+const popup = page.querySelector(".popup");
+const closeIcon = popup.querySelector(".popup__close-icon");
 
-$(function(){
-  jQuery.fn.swap = function(b) {
+$(function () {
+  jQuery.fn.swap = function (b) {
     b = jQuery(b)[0];
     var a = this[0],
-        a2 = a.cloneNode(true),
-        b2 = b.cloneNode(true),
-        stack = this;
+      a2 = a.cloneNode(true),
+      b2 = b.cloneNode(true),
+      stack = this;
 
     a.parentNode.replaceChild(b2, a);
     b.parentNode.replaceChild(a2, b);
 
     stack[0] = a2;
-    return this.pushStack( stack );
-};
+    return this.pushStack(stack);
+  };
 
-$('#button_type_success').on('click', function(){
-  $('.left').swap('.right');
-});
+  $("#button_type_success").on("click", function () {
+    $(".left").swap(".right");
   });
+});
 
 const closeModal = (evt) => {
-  if (evt.key === 'Escape') {
+  if (evt.key === "Escape") {
     closePopup();
   }
 };
 
 const overlayClick = (evt) => {
-  if (evt.target.classList.contains('popup')) {
+  if (evt.target.classList.contains("popup")) {
     closePopup();
   }
-}
+};
 const closePopup = () => {
-  popup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closeModal);
-  document.removeEventListener('mousedown', overlayClick);
-}
+  popup.classList.remove("popup_opened");
+  document.removeEventListener("keydown", closeModal);
+  document.removeEventListener("mousedown", overlayClick);
+};
 
-buttonWarning.addEventListener('click', () => {
-    header.classList.toggle('header_hide');
+buttonWarning.addEventListener("click", () => {
+  header.classList.toggle("header_hide");
 });
-closeIcon.addEventListener('click', () => {
+closeIcon.addEventListener("click", () => {
   closePopup();
 });
-document.addEventListener('keydown', closeModal);
-document.addEventListener('mousedown', overlayClick);
+document.addEventListener("keydown", closeModal);
+document.addEventListener("mousedown", overlayClick);
