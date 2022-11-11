3214e9a7a5c7e7c884e299a351616a6e106680cb
auroraptor <interestingbutno@yandex.ru>
2022-11-11 11:58:05 +0300

    fix: реализация свапа блоков через css order

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,10 @@
 const page = document.querySelector(".page");
 const header = page.querySelector(".header");
 const buttonWarning = page.querySelector("#button_type_warning");
+const buttonSuccess = page.querySelector("#button_type_success");
+const left = page.querySelector('.left');
 const popup = page.querySelector(".popup");
 const closeIcon = popup.querySelector(".icon");
-
-// TODO это можно сделать CSS flex order
-$(function () {
-  jQuery.fn.swap = function (b) {
-    b = jQuery(b)[0];
-    var a = this[0],
-      a2 = a.cloneNode(true),
-      b2 = b.cloneNode(true),
-      stack = this;
-
-    a.parentNode.replaceChild(b2, a);
-    b.parentNode.replaceChild(a2, b);
-
-    stack[0] = a2;
-    return this.pushStack(stack);
-  };
-
-  $("#button_type_success").on("click", function () {
-    $(".left").swap(".right");
-  });
-});
 
 const closePopup = () => {
   popup.classList.remove("popup_opened");
@@ -46,7 +27,9 @@ const overlayClick = (evt) => {
 buttonWarning.addEventListener("click", () => {
   header.classList.toggle("hidden");
 });
-
+buttonSuccess.addEventListener("click", () => {
+    left.style.order = left.style.order == 0 ? 1 : 0;
+})
 closeIcon.addEventListener("click", closePopup);
 document.addEventListener("keydown", closeModal);
 document.addEventListener("mousedown", overlayClick);
