5987f5dad3bbba4d5b60a59a8d4829e7eeb96780
auroraptor <interestingbutno@yandex.ru>
2022-11-11 11:59:08 +0300

    prettier

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const page = document.querySelector(".page");
 const header = page.querySelector(".header");
 const buttonWarning = page.querySelector("#button_type_warning");
 const buttonSuccess = page.querySelector("#button_type_success");
-const left = page.querySelector('.left');
+const left = page.querySelector(".left");
 const popup = page.querySelector(".popup");
 const closeIcon = popup.querySelector(".icon");
 
@@ -28,8 +28,8 @@ buttonWarning.addEventListener("click", () => {
   header.classList.toggle("hidden");
 });
 buttonSuccess.addEventListener("click", () => {
-    left.style.order = left.style.order == 0 ? 1 : 0;
-})
+  left.style.order = left.style.order == 0 ? 1 : 0;
+});
 closeIcon.addEventListener("click", closePopup);
 document.addEventListener("keydown", closeModal);
 document.addEventListener("mousedown", overlayClick);
